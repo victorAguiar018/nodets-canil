f5c9e1e59a014656da66e63cb0183ea385f84ea5
victoraguiar <victoraguiar018@gmail.com>
2023-09-05 21:28:03 -0300

    adicionando funcionalidades

--- src/controllers/pageControler.ts
+++ src/controllers/pageControler.ts
@@ -1,18 +1,39 @@
 import { Request, Response } from "express";
+import { createMenuObject } from '../helpers/createMenuObject'
 
 export const home = (req: Request, res: Response) =>{
-    res.send('home no controller')
-    //res.render('pages/page);
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            tittle: 'Todos os Animais',
+            background: 'allanimals.jpg'
+        }
+    })
 }
 export const dogs = (req: Request, res: Response) =>{
-    
-    //res.render('pages/page);
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            tittle: 'Cachorro',
+            background: 'banner_dog.jpg'
+        }
+    })
 }
 export const cats = (req: Request, res: Response) =>{
-    
-    //res.render('pages/page);
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            tittle: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
 }
 export const fishes = (req: Request, res: Response) =>{
-    
-    //res.render('pages/page);
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            tittle: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
 }
